2eb243ff5c042038a7f48a26a1b15341b3c4a539
raaaahman <sylvain@devindetails.com>
2024-07-19 21:27:58 +0200

    tool(router): Add Tanstack Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "development"
+    ? React.lazy(() =>
+        import("@tanstack/router-devtools").then((response) => ({
+          default: response.TanStackRouterDevtools,
+        }))
+      )
+    : () => null;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+    <TanStackRouterDevtools router={router} />
+  </React.StrictMode>
+);
