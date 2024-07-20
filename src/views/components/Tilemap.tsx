2bc695e249b8c24e86ec5b7959283297837d4f5a
raaaahman <sylvain@devindetails.com>
2024-07-20 16:44:57 +0200

    feat(Tilemap): Make tiles rotate on click

--- src/views/components/Tilemap.tsx
+++ src/views/components/Tilemap.tsx
@@ -1,4 +1,4 @@
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import {
   PositionComponent,
   RotationComponent,
@@ -9,14 +9,29 @@ import { useQuery } from "../hooks/useQuery";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 import { Clone } from "@react-three/drei";
+import { ComponentProps, useRef, useState } from "react";
 
-export function Tilemap() {
+export function Tilemap({ ...props }: ComponentProps<"group">) {
   const tiles = useQuery(tilesQuery);
 
   const tileset = useLoader(GLTFLoader, "/models/road_tileset.glb");
 
+  const commandsQueue = useRef<number[]>([]);
+
+  const [updated, forceUpdate] = useState(false);
+
+  useFrame(() => {
+    while (commandsQueue.current.length > 0) {
+      const eid = commandsQueue.current.pop() as number;
+
+      RotationComponent.z[eid] = RotationComponent.z[eid] + (1 % 4);
+
+      if (commandsQueue.current.length === 0) forceUpdate(!updated);
+    }
+  });
+
   return (
-    <>
+    <group {...props}>
       {tiles.map((eid) => (
         <Clone
           key={eid}
@@ -27,9 +42,13 @@ export function Tilemap() {
             PositionComponent.z[eid],
           ]}
           rotation-z={(RotationComponent.z[eid] * Math.PI) / 2}
+          onClick={() => {
+            if (!commandsQueue.current.includes(eid))
+              commandsQueue.current.push(eid);
+          }}
         />
       ))}
-    </>
+    </group>
   );
 }
 
