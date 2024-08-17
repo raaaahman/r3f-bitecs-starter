693eb73519acca5c36ded3ccbebc8449a1deac1b
raaaahman <sylvain@devindetails.com>
2024-08-17 11:38:56 +0200

    refactor(types): Extract utility types from world type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,18 @@
 import { IWorld } from "bitecs";
 
-export type WorldWithTime = IWorld & {
+export type WithTime<T extends IWorld> = T & {
   time: {
     delta: number;
     elapsed: number;
     then: number;
   };
 };
+
+export type WithBoundaries<T extends IWorld> = T & {
+  size: {
+    width: number;
+    height: number;
+  };
+};
+
+export type CustomWorld = WithTime<WithBoundaries<IWorld>>;
